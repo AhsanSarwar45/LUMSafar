3e1057830f7e14dfa96136ab45eee16aa352a797
Omer Kamran <omer.kamran100@gmail.com>
2022-04-04 20:26:32 +0500

    Update users.js

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -19,4 +19,29 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 })
 
+router.route('/:id').get((req, res) => {
+    User.findById(req.params.id)
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            user.email = req.body.email;
+            user.password = req.body.password;
+            user.isSociety = req.body.isSociety;
+
+            user.save()
+                .then(() => res.json('User updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 module.exports = router;
