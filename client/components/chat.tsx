8c404b434a9d97d7dd6f1dbd4e41d9655f4d5071
Saood-21 <saood5858450@gmail.com>
2022-04-16 20:51:55 +0500

    chat engine

---
+++ client/components/chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='saood'
+			userSecret='123'
+			projectID='fdf27536-3df7-4058-a201-0b02fbf7d131'
+		/>
+	);
+}
