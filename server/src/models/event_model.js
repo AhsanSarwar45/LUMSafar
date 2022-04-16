fdf8d2d7c2e7da3b796ca18ae2e637d2d8f9eb87
Omer Kamran <omer.kamran100@gmail.com>
2022-04-16 21:01:17 +0500

    Fix package name

--- server/src/models/event_model.js
+++ server/src/models/event_model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoose_fuzzy_searching = require('mongoose-fuzzy-searching');
+const mongoose_fuzzy_searching = require('mongoose-fuzzy-search');
 const user = require('./user_model.js');
 
 const eventSchema = new mongoose.Schema({
